baa54af9954af9d5743608fbf7d4223531d40d40
hwangdaesun <hds650588@gmail.com>
2023-06-24 01:05:56 +0900

    close #3

---
+++ src/main/java/com/theZ/dotoring/DotoringApplication.java
@@ -0,0 +1,13 @@
+package com.theZ.dotoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotoringApplication.class, args);
+	}
+
+}
